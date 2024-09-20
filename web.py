aaf0c62cf08483a9198fdcbc9fbece889d2d9578
CodeLearners <89776792+ealion@users.noreply.github.com>
2024-09-21 00:16:22 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,15 +1,5 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-# Streamlit app
-def main():
-    st.title("Simple WebRTC Streamlit Example")
 
-    # Create a simple WebRTC video streamer
-    webrtc_streamer(
-        key="simple_example",
-        # No need for video_transformer_factory or other parameters
-    )
-
-if __name__ == "__main__":
-    main()
+webrtc_streamer(key="simple_example")
