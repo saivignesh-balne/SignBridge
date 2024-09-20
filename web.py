c82a77b0da291b8708ce8d81916edc7f65f7aa3f
CodeLearners <89776792+ealion@users.noreply.github.com>
2024-09-21 00:23:37 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,5 +1,15 @@
+import av
+import cv2
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
+st.title("Computer Vision Streamlit application")
 
-webrtc_streamer(key="simple_example")
+class VideoProcessor:
+    def recv(self, frame):
+        image = frame.to_ndarray(format="bgr24")
+        image = cv2.cvtColor(cv2.Canny(image, 100, 200), cv2.COLOR_GRAY2BGR)
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
+
+
+webrtc_streamer(key="demo", video_processor_factory=VideoProcessor)
